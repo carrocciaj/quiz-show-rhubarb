cd1475ff23e458e098283ebfcadc58f07b9aa9f1
Josh Carroccia <agr.carroccia@gmail.com>
2018-09-23 14:08:41 -0400

    built jbuilder for quiz show

--- backend/app/controllers/api/quizzes_controller.rb
+++ backend/app/controllers/api/quizzes_controller.rb
@@ -2,12 +2,13 @@ class Api::QuizzesController < ApplicationController
     before_action :set_quiz, only: [:show, :destroy, :update]
 
     def index
-        @quizzes = Quiz.all
+        @quizzes = Quiz.where(published: true)
         render json: @quizzes
     end
 
     def show
-        render json: @quiz
+        @questions = @quiz.questions.includes(:answers) 
+       
     end
 
     def create
