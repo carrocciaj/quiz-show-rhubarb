3041f14d5dc3dab735e72546e4a00617a64c19d0
Josh Carroccia <agr.carroccia@gmail.com>
2018-09-23 11:25:28 -0400

    changing port origin to 3000

--- backend/config/application.rb
+++ backend/config/application.rb
@@ -23,7 +23,7 @@ module Backend
     config.load_defaults 5.2
     config.middleware.insert_before 0, Rack::Cors do
       allow do
-        origins 'http://localhost:3001'
+        origins 'http://localhost:3000'
         resource '*', :headers => :any, :methods => [:get, :post, :options]
       end
     end
