e6539898f7fcf8b6ce2ff4c27662c90179ba0ad3
saraharneson <sarah.m.arneson@gmail.com>
2018-09-22 22:37:24 -0400

    api progress and need to close to fixing registration button. Look at fields and errors.

--- frontend/src/LoginArea.js
+++ frontend/src/LoginArea.js
@@ -1,8 +1,9 @@
 import React from 'react'
 
 // import { ... } from 'react-router-dom'
-// import data from './data'
-import PropTypes from 'prop-types'
+import data from './data'
+import QuizBoard from './QuizBoard'
+// import PropTypes from 'prop-types'
 import 'bulma/css/bulma.css'
 import './App.css'
 
@@ -20,14 +21,14 @@ class LoginArea extends React.Component {
   handleSubmit (event) {
     event.preventDefault()
     console.log(this.state.username, 'username')
-    // const { username, password } = this.state
-    // // data.login(username, password)
-    //   .then(user => this.props.setCurrentUser(user))
-    //   .catch(err => {
-    //     this.setState({
-    //       errorMsg: err.errorMessage
-    //     })
-    //   })
+    const { username, password } = this.state
+    data.login(username, password)
+      .then(user => this.props.setCurrentUser(user))
+      .catch(err => {
+        this.setState({
+          errorMsg: err.errorMessage
+        })
+      })
   }
 
   render () {
@@ -35,38 +36,38 @@ class LoginArea extends React.Component {
 
     return (
       <React.Fragment>
-        <div className='login-view'>
-          <div className='main-heads'>Log in</div>
-          <form>
-            <div className='field'>
-              <label className='label'>Username</label>
-              <div className='control'>
-                <input className='input' type='text' placeholder='username' onChange={(e) => this.setState({ username: e.target.value })} />
+        <QuizBoard>
+
+          <div className='login-view'>
+            <div className='main-heads'>Log in</div>
+            <form>
+              <div className='field'>
+                <label className='label'>Username</label>
+                <div className='control'>
+                  <input className='input' type='text' placeholder='username' onChange={(e) => this.setState({ username: e.target.value })} />
+                </div>
               </div>
-            </div>
-            <div className='field'>
-              <label className='label'>Password</label>
-              <div className='control'>
-                <input className='input' type='password' placeholder='password' onChange={(e) => this.setState({ password: e.target.value })} />
+              <div className='field'>
+                <label className='label'>Password</label>
+                <div className='control'>
+                  <input className='input' type='password' placeholder='password' onChange={(e) => this.setState({ password: e.target.value })} />
+                </div>
               </div>
-              <button className='button is-primary login-submit' type='submit' onClick={(e) =>
-                this.handleSubmit(e)
-              }
-              >Login</button>
+              <button className='button is-primary login-submit' type='submit' onClick={(e) => this.handleSubmit(e)} >Login</button>
               <div className='registration-option'>
                 <p className='registration-question'> Don't have an account? </p>
                 <button className='button is-primary register-link'>Register</button>
               </div>
-            </div>
-          </form>
-        </div>
+            </form>
+          </div>
+        </QuizBoard>
       </React.Fragment>
     )
   }
 }
 
-LoginArea.propTypes = {
-  setCurrentUser: PropTypes.func.isRequired
-}
+// LoginArea.propTypes = {
+//   setCurrentUser: PropTypes.func.isRequired
+// }
 
 export default LoginArea
--- frontend/src/data.js
+++ frontend/src/data.js
@@ -2,7 +2,7 @@ import request from 'superagent'
 
 let userToken
 const apiDomain = process.env.REACT_APP_API_DOMAIN
-// const apiDomain = REACT_APP_API_DOMAIN=https://rhubarb-quiz.herokuapp.com/
+// const apiDomain = REACT_APP_API_DOMAIN=https://rhubarb-quiz.herokuapp.com
 
 const data = {
   setUserToken: (token) => {
@@ -11,23 +11,34 @@ const data = {
   getUserToken: () => {
     return userToken
   },
-  
-login: (username, password) => {
-  return request.post(`${apiDomain}/api/login`)
-    .send({ username, password })
-    .then(res => res.body.api_token)
-    .then( token => {
-      data.setUserToken(token)
-      return { username, token }
-    })
-    // COME BACK TO THIS PART BELOW:
+
+  login: (username, password) => {
+    return request.post(`${apiDomain}/api/login`)
+      .send({ username, password })
+      .then(res => res.body.api_token)
+      .then(token => {
+        data.setUserToken(token)
+        return { username, token }
+      })
+
     // .catch(err) => {
     //   if (err.response.statusCode
-    //   Come back to this
+    //   Come back to this !!
+    // }
+  },
+  register: (username, password) => {
+    return request.post(`${apiDomain}/api/users`)
+      .send({ username, password })
+      .then(res => res.body)
+      .then(user => {
+        data.setUserToken(user.token)
+        return user
+      })
+    // .catch(err) => {
+    //   if (err.response.statusCode
+    //   Come back to this !!
     // }
   }
-
-  getUserToken 
-
+  // getUserToken
 }
 export default data
