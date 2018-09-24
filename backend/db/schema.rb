dd98fe6f1bc6646acec21c00298fb20db46901d5
Joanna Davidson <joanna@shrikesong.com>
2018-09-24 14:58:26 -0400

    Generated Score model. Scores ctrlr calcs scores  and saves submissions

--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_09_22_161749) do
+ActiveRecord::Schema.define(version: 2018_09_24_184258) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -37,6 +37,15 @@ ActiveRecord::Schema.define(version: 2018_09_22_161749) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.boolean "published", default: false
+  end
+
+  create_table "scores", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "quiz_id"
+    t.integer "score"
+    t.string "submission"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
