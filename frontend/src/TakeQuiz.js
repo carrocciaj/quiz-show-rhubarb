745f5cdfa5fb6915274239c6feeac9f5231a9424
saraharneson <sarah.m.arneson@gmail.com>
2018-09-25 23:15:51 -0400

    last commit of Tuesday night

--- frontend/src/TakeQuiz.js
+++ frontend/src/TakeQuiz.js
@@ -3,6 +3,7 @@ import 'bulma/css/bulma.css'
 import './App.css'
 import { BrowserRouter as Redirect } from 'react-router-dom'
 import data from './data'
+import { callbackify } from 'util';
 
 class TakeQuiz extends Component {
   constructor (props) {
@@ -14,7 +15,7 @@ class TakeQuiz extends Component {
     }
     this.populateQuiz = this.populateQuiz.bind(this)
     this.handleChange = this.handleChange.bind(this)
-    this.handleSubmit = this.handleSubmit.bind(this)
+    // this.handleSubmit = this.handleSubmit.bind(this)
   }
 
   componentDidMount () {
@@ -33,37 +34,51 @@ class TakeQuiz extends Component {
     this.setState(gatheredAnswers)
   }
 
+  // The below is throwing an error and needs to be fixed to make API call.
+
+  // handleSubmit () {
+  //   event.preventDefault()
+  //   // call the submitAnswers function/API call from .data file? Not sure how.
+  //   submitAnswers
+  // }
+
   render () {
     if (!this.props.currentUser) {
       return <Redirect to='/login' />
     }
 
-    handleSubmit (event) {
-      event.preventDefault()
-      //call the submitAnswers function/API call from .data file? Not sure how.
-      submitAnswers
-    }
-
     const quizData = this.state.quiz
     if (quizData.data) {
       return (
-        <div>
-          <ol>
+        <div className='current-quiz'>
+          <div className='quiz quiz-head'>{quizData.data.title}</div>
+          <ol className='QandA-unit'>
             {/* {/* Not sure about the input fields event handlers - functions above - } */}
             { quizData.data.questions.map((question, idx) =>
               <li key={idx}>
-                {question.text}
-                {question.answers.map((answer, idx) =>
-                  <div key={idx}>
-                    <input type='radio' id={answer.id}
-                      name={question.id} value={answer.id} checked={quizData.answer} onChange={this.handleChange} />
-                    <label htmlFor={answer.id}>{answer.text}</label>
-                  </div>
-                )}
+                <div className='quiz-questions'>{question.text}</div>
+                <div className='quiz-answers'>
+                  {question.answers.map((answer, idx) =>
+                    <div key={idx}>
+
+                      {/* Code I had before, with attempted edit from Amy below  ... */}
+
+                      {/* <input type='radio' id={answer.id}
+                      name={question.id} value={answer.id} checked={quizData.answer} onChange={this.handleChange} /> */}
+                      {/* Re: below. it wouldn't let me make checked={true} as done in that article and rec'd by Amy ... need to assign that something */}
+                      <div class='control'>
+                        <input type='radio' id={answer.id}
+                          name={question.id} value={answer.id} checked onChange={this.handleChange} />
+                        <label className='radio answers' htmlFor={answer.id}>{answer.text}</label>
+                      </div>
+                    </div>
+                  )}
+                </div>
               </li>
             )}
           </ol>
-          <button className='button is-primary score-submit' type='submit' onClick={ handleSubmit(e) }>Score me!</button>
+
+          <button className='button is-primary score-submit' type='submit' onClick={(e) => this.handleSubmit}>Score me!</button>
         </div>
       )
     } else {
